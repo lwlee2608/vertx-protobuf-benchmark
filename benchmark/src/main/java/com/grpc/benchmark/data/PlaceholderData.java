589f51d44c0b15405f0d2cbd6a7b353303232050
Ong Jack Hoe <ongjackhoe@gmail.com>
2023-10-16 19:49:29 +0800

    Update `model` module and added github workflow to test build (#1)
    * Update model module and maven plugins to gen .proto and gen grpc model
    
    * Update maven compiler plugin execution namings
    
    * Added github action for build test
    
    * Rename workflow branch name
    
    * Remove github workflow
    
    * Revert "Remove github workflow"
    
    This reverts commit 13442d1bea225338bc10dd6b69dcd7339ec21087.
    
    * Added snapshot remote repo
    
    * Rename job name for github actions

--- benchmark/src/main/java/com/grpc/benchmark/data/PlaceholderData.java
+++ benchmark/src/main/java/com/grpc/benchmark/data/PlaceholderData.java
@@ -2,20 +2,22 @@ package com.grpc.benchmark.data;
 
 import io.github.lwlee2608.benchmark.model.TestSubject;
 import io.vertx.core.json.JsonObject;
+import io.vertx.protobuf.generated.Dataobjects;
 
 public class PlaceholderData {
-//    static public TestSubject getProtoPayload() {
-//        return TestSubject.newBuilder()
-//                .setIntField(42)
-////                .setStringField("Test String")
-//                .setBoolField(true)
-//                .setDoubleField(42.42)
-////                .setBytesField(ByteString.copyFromUtf8("Test Bytes"))
-//                .setIntField2(420)
-//                .setIntField3(42000)
-//                .setIntField4(4200000)
-//                .build();
-//    }
+    static public io.vertx.protobuf.generated.TestSubject getProtoPayload() {
+
+        return io.vertx.protobuf.generated.TestSubject.newBuilder()
+                .setIntField(42)
+//                .setStringField("Test String")
+                .setBoolField(true)
+                .setDoubleField(42.42)
+//                .setBytesField(ByteString.copyFromUtf8("Test Bytes"))
+                .setIntField2(420)
+                .setIntField3(42000)
+                .setIntField4(4200000)
+                .build();
+    }
 
     static public TestSubject getTestSubjectPojo() {
         TestSubject payload = new TestSubject();
