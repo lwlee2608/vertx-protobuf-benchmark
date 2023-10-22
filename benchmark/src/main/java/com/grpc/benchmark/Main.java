bb43e0ba9bf7ed44456d778f33baab3b2cbf3621
Ong Jack Hoe <ongjackhoe@gmail.com>
2023-10-22 10:44:17 +0800

    Update jmh version and added async profiler to run along side (#2)

--- benchmark/src/main/java/com/grpc/benchmark/Main.java
+++ benchmark/src/main/java/com/grpc/benchmark/Main.java
@@ -1,5 +1,12 @@
 package com.grpc.benchmark;
 
+import com.grpc.benchmark.scenario.JsonBenchmark;
+import com.grpc.benchmark.scenario.ProtoGoogleBenchmark;
+import com.grpc.benchmark.scenario.ProtoVertxBenchmark;
+import org.openjdk.jmh.profile.AsyncProfiler;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +14,13 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main( String[] args ) throws Exception {
-        org.openjdk.jmh.Main.main(args);
+        Options opt = new OptionsBuilder()
+                .include(ProtoGoogleBenchmark.class.getSimpleName())
+                .include(JsonBenchmark.class.getSimpleName())
+                .include(ProtoVertxBenchmark.class.getSimpleName())
+                .addProfiler(AsyncProfiler.class, "output=flamegraph;dir=/tmp/")
+                .forks(1)
+                .build();
+        new Runner(opt).run();
     }
 }
